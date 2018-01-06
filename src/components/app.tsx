40e0c22113f9e5d2fdee2982a134b07f4d13c494
kgtkr <kgtkr.jp@gmail.com>
2018-01-06 21:32:47 +0900

    端を選択できるようにした

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -9,7 +9,11 @@ import {
   Observable
 } from "rxjs";
 import * as utils from "../utils";
-import { Slider } from "material-ui";
+import {
+  Slider,
+  SelectField,
+  MenuItem
+} from "material-ui";
 import * as spline from "@yr/catmull-rom-spline";
 
 interface AppProps {
@@ -89,81 +93,100 @@ export class App extends React.Component<AppProps, AppState> {
     const height = 500;
     const vbh = 10;
 
-    return <div style={{
-      display: "flex"
-    }}>
-      <Slider
-        axis="y"
-        value={this.state.mouseY}
-        onChange={(_e, v) => this.setState({ mouseY: v })}
-        min={-vbh / 2}
-        max={vbh / 2}
-        step={vbh / 100}
-        style={{ height }}
-        sliderStyle={{ margin: 0 }} />
-      <svg width={width}
-        height={height}
-        viewBox={`0 ${-vbh / 2} ${this.state.machine.w} ${vbh}`}>
-        {//波形
-          (() => {
-            const [d, d1, d2] = this.pahtD();
-            return <g>
-              <path
-                fill="transparent"
-                stroke="red"
-                strokeWidth={0.02}
-                d={d1} />
-              <path
-                fill="transparent"
-                stroke="blue"
-                strokeWidth={0.02}
-                d={d2} />
-              <path
-                fill="transparent"
-                stroke="black"
-                strokeWidth={0.03}
-                d={d} />
-            </g>;
-          })()
-        }
-        <g stroke="blue" >
-          {
-            //縦グリッド
-            utils.range(0, Math.floor(this.state.machine.w))
-              .map(x => <line
-                key={x}
-                x1={x}
-                y1={vbh / 2}
-                x2={x}
-                y2={-vbh / 2}
-                strokeWidth={0.01} />)}
-          {
-            //横グリッド
-            utils.range(-Math.floor(vbh / 2), Math.floor(vbh / 2))
-              .map(y => <line
-                key={y}
-                x1={0}
-                y1={y}
-                x2={this.state.machine.w}
-                y2={y}
-                strokeWidth={0.01} />)}
-          {
-            //中央線
+    return <div>
+      <div style={{
+        display: "flex"
+      }}>
+        <Slider
+          axis="y"
+          value={this.state.mouseY}
+          onChange={(_e, v) => this.setState({ mouseY: v })}
+          min={-vbh / 2}
+          max={vbh / 2}
+          step={vbh / 100}
+          style={{ height }}
+          sliderStyle={{ margin: 0 }} />
+        <svg width={width}
+          height={height}
+          viewBox={`0 ${-vbh / 2} ${this.state.machine.w} ${vbh}`}>
+          {//波形
+            (() => {
+              const [d, d1, d2] = this.pahtD();
+              return <g>
+                <path
+                  fill="transparent"
+                  stroke="red"
+                  strokeWidth={0.02}
+                  d={d1} />
+                <path
+                  fill="transparent"
+                  stroke="blue"
+                  strokeWidth={0.02}
+                  d={d2} />
+                <path
+                  fill="transparent"
+                  stroke="black"
+                  strokeWidth={0.03}
+                  d={d} />
+              </g>;
+            })()
           }
-          <line x1={0} y1={0} x2={this.state.machine.w} y2={0} strokeWidth={0.02} />
-        </g>
-        {
-          //波生成線
-        }
-        <g stroke="red" >
-          <line
-            x1={0}
-            y1={-this.state.mouseY}
-            x2={this.state.machine.w}
-            y2={-this.state.mouseY}
-            strokeWidth={0.01} />
-        </g>
-      </svg>
+          <g stroke="blue" >
+            {
+              //縦グリッド
+              utils.range(0, Math.floor(this.state.machine.w))
+                .map(x => <line
+                  key={x}
+                  x1={x}
+                  y1={vbh / 2}
+                  x2={x}
+                  y2={-vbh / 2}
+                  strokeWidth={0.01} />)}
+            {
+              //横グリッド
+              utils.range(-Math.floor(vbh / 2), Math.floor(vbh / 2))
+                .map(y => <line
+                  key={y}
+                  x1={0}
+                  y1={y}
+                  x2={this.state.machine.w}
+                  y2={y}
+                  strokeWidth={0.01} />)}
+            {
+              //中央線
+            }
+            <line x1={0} y1={0} x2={this.state.machine.w} y2={0} strokeWidth={0.02} />
+          </g>
+          {
+            //波生成線
+          }
+          <g stroke="red" >
+            <line
+              x1={0}
+              y1={-this.state.mouseY}
+              x2={this.state.machine.w}
+              y2={-this.state.mouseY}
+              strokeWidth={0.01} />
+          </g>
+        </svg>
+      </div>
+      <div>
+        <SelectField
+          floatingLabelText="端"
+          value={this.state.machine.end}
+          onChange={(_e, _i, v) => this.setState({
+            ...this.state,
+            machine: {
+              ...this.state.machine,
+              end: v
+            }
+          })}
+        >
+          <MenuItem value="fixed" primaryText="固定端" />
+          <MenuItem value="free" primaryText="自由端" />
+          <MenuItem value="none" primaryText="反射しない" />
+        </SelectField>
+      </div>
     </div>;
   }
 }
